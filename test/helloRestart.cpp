0ebe6e0e4215baad2a36effec0a22be013dd1d60
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-12-09 16:45:45 +0100

    trying different approach for output

--- test/helloRestart.cpp
+++ test/helloRestart.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
+// using std ::cout;
+// using std :: endl;
 
 using namespace std;
 
 int main (){
     cout << "Restarted practice of cpp again";
+   // cout << "Restarted practice of cpp again" << endl;  when using std differently
 }
