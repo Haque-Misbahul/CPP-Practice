8dfabaf250b91677c2991852a82a3867c8ac06e2
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-12-15 23:29:31 +0100

    variable and constants

--- 01_varconst/main.cpp
+++ 01_varconst/main.cpp
@@ -4,7 +4,18 @@
 using namespace std;
 
 int main(){
-    cout << "Welcome 1" << endl;
+
+    int score;  //declare
+    score = 110;  // initialize
+
+    const int uid = 123456;
+
+    int balance = 500; // declare and initialize
+
+    balance = 1000;
+
+
+    cout << "Welcome 1" << endl; 
     cout << "Welcome 2" << endl;
     cout << "Welcome 3" << endl;
     return 0;
