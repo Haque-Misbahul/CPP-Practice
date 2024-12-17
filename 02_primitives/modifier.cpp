be06d4f43ebcbd7b252e3a694683740cc4edfb22
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-12-17 23:00:03 +0100

    modifier

---
+++ 02_primitives/modifier.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+
+    unsigned smallTeaPack = 100;
+    long long largeTeaStorage = 100000;
+    short teaSample = 25;
+
+
+    cout << smallTeaPack << endl;
+
+    return 0;
+}
