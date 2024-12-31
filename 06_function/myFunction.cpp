fae7d1ca4525bc5235ff6a8f90b440e2f03b28b9
Haque-Misbahul <mhaque.tuc@gmail.com>
2024-12-31 19:52:28 +0100

    functiona declaration

--- 06_function/myFunction.cpp
+++ 06_function/myFunction.cpp
@@ -2,3 +2,19 @@
 // function body
 // void
 //}
+
+#include <iostream>
+using namespace std;
+
+void serveChai(int cups);
+
+int main() {
+    serveChai(3); // Function call
+    return 0;
+}
+// Function definition before main
+void serveChai(int cups) {
+    cout << "Serving " << cups << " cups of chai \n";
+}
+
+
