c8fa88dba75517994a6ac24a1fa249121fc7789b
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-01-02 02:02:15 +0100

    object created

--- 08_oop/myClass.cpp
+++ 08_oop/myClass.cpp
@@ -5,30 +5,37 @@ using namespace std;
 
 
 class Chai{
-//data members/(attributes)
+    //data members/(attributes)
+    public:
+        string teaName;   //name of the tea
+        int servings;  // number of servings
+        vector<string> ingredients;  // list of ingridients
 
-string teaName;   //name of the tea
-int servings;  // number of servings
-vector<string> ingredients;  // list of ingridients
-
-
-// memeber function
-
-void displayChaiDetails (){
-    cout << "Tea Name: "  << teaName << endl;
-    cout << "Servubgs: " << servings << endl;
-    cout << "Ingerdients: " ;
-    for (string ingredient : ingredients)
-    {
-        cout << ingredient << " ";
-    }
-    cout << endl;
-    
-}
+        // memeber function
+        void displayChaiDetails (){
+            cout << "Tea Name: "  << teaName << endl;
+            cout << "Servibgs: " << servings << endl;
+            cout << "Ingerdients: " ;
+            for (string ingredient : ingredients)
+            {
+                cout << ingredient << " ";
+            }
+            cout << endl;
+            
+        }
 
 };
 
 
 int main (){
+
+Chai chaiOne;
+
+chaiOne.teaName = "lemon tea";
+chaiOne.servings = 2;
+chaiOne.ingredients = {"water", "lemon", "honey", "tea"};
+
+chaiOne.displayChaiDetails();
+
 return 0;
 }
