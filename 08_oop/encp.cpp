32a135bc685e235243811d8465182a96603bac67
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-01-16 23:18:15 +0100

    Created bank acc class

--- 08_oop/encp.cpp
+++ 08_oop/encp.cpp
@@ -1,1 +1,26 @@
-#include<iostream>
+#include <iostream>
+
+using namespace std;
+
+class BankAccount {
+    private:
+        string accountNumber;
+        double balance;
+
+    public:
+        BankAccount(string accNum, double initialBalance){
+            accountNumber = accNum;
+            balance = initialBalance;
+        }
+
+      
+
+};
+
+int main(){
+    BankAccount myAccount("1229988", 500);
+
+  
+
+    return 0;
+}
