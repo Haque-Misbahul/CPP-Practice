bb6fd147750d688c4769fd077cdb7833bc379ad1
Haque-Misbahul <mhaque.tuc@gmail.com>
2025-01-18 15:25:38 +0100

    abstract class

--- 08_oop/inheritance.cpp
+++ 08_oop/inheritance.cpp
@@ -1,9 +1,26 @@
 #include<iostream>
 #include<string>
+#include<vector>
 
 
 using namespace std;
 
+//base/parent class
+
+class Tea{
+    protected:
+        string teaName;
+        int servings;
+
+    public:
+        Tea(string name, int serve): teaName(name), servings(serve){
+            cout << "Tea constructor called" << teaName << endl;
+        }
+
+        virtual void brew() const {
+            cout << "Brewing " << teaName << "With generic method" << endl;
+        }
+};
 
 
 int main(){
